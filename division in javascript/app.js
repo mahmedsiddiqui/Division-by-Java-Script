4dcb650a80da84b85f70b37732e5a6bb15109a8e
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 11:02:40 +0500

    Add files via upload

---
+++ division in javascript/app.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter Your First Number To Divide'));
+const num2 = parseInt(prompt('Enter Your Second Number To Divide '));
+const sum = num1 / num2;
+document.write(`Your Sum is ${sum}`); 
